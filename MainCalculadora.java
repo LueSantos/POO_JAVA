8d699385995e7b79a1cce3ae9343668380bb5df8
LueSantos <86605122+LueSantos@users.noreply.github.com>
2021-09-28 18:46:28 -0300

    update

---
+++ MainCalculadora.java
@@ -0,0 +1,12 @@
+package exercio2;
+import java.util.Scanner;
+
+public class MainCalculadora {
+
+	public static void main(String[] args) {
+		
+		Calculadora  cal = new Calculadora();
+		cal.Calculo();
+		
+	}
+}
