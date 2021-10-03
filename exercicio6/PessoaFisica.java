c6c87117fd1d759eb6bbb7d3c82bde3d02408368
LueSantos <86605122+LueSantos@users.noreply.github.com>
2021-10-03 11:45:34 -0300

    Update PessoaFisica.java

--- exercicio6/PessoaFisica.java
+++ exercicio6/PessoaFisica.java
@@ -2,17 +2,19 @@ package aula;
 
 public class PessoaFisica extends ImpostoDeRenda {
 	
+	// Atributos 
+	
 	private String cpf;
 	private String rg;
 	
+	// Constructor com herança;
 	public PessoaFisica(String nome, double rendimentos, String cpf, String rg) {
 		super(nome, rendimentos);
 		this.cpf = cpf;
 		this.rg = rg;
 	}
 
-
-	
+	// Sobrescrição de método formatado do objeto;
 	@Override
 	public String toString() {
 		return String.format("Pessoa Fisica - nome: %s, rendimentos: %.2f, cpf: %s, rg: %s",
