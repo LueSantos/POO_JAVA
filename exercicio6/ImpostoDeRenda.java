38a01b03cfe931067c6ddb896b384f761daa84d7
LueSantos <86605122+LueSantos@users.noreply.github.com>
2021-10-03 11:42:47 -0300

    Update ImpostoDeRenda.java

--- exercicio6/ImpostoDeRenda.java
+++ exercicio6/ImpostoDeRenda.java
@@ -2,18 +2,23 @@ package aula;
 
 public class ImpostoDeRenda {
 	
+	//Atributos da classe com PROTECTED;
+
 	protected String nome;
 	protected String telefone;
 	protected String email; 
 	protected double rendimentos;
 	
 	
+	//Construtor da classe;
+	
 	public ImpostoDeRenda(String nome, double rendimentos) {
 		this.nome = nome;
 		this.rendimentos = rendimentos;
 	}
 	
-	
+	//Método genérico que será sobrescrito nas outras classes;
+
 	public void calculoIR() {
 		this.rendimentos = this.rendimentos - (this.rendimentos * 0.12);
 			
