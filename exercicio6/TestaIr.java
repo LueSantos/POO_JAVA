9b1c3076d6d370c17a392dbe98bc1a01aaf0fea7
LueSantos <86605122+LueSantos@users.noreply.github.com>
2021-10-03 11:53:53 -0300

    Update TestaIr.java

--- exercicio6/TestaIr.java
+++ exercicio6/TestaIr.java
@@ -4,13 +4,17 @@ public class TestaIr {
 
 	public static void main(String[] args) {
 
+		// Instâncias das classes;
 		
 		PessoaFisica PF = new PessoaFisica("Luciana Santos", 420.000, "654890772-31", "34521");
 		PessoaJuridica PJ = new PessoaJuridica("LarxShoes", 120.000, "548907/0001-43", "3452156-2");
 
+		// Chamada do método de para calcular IR; 
 		
 		PF.calculoIR();
 		PJ.calculoIR();
+		
+		// Impressão dos objetos formataados --> toString; 
 		
 		System.out.println(PF);
 		System.out.println(PJ);
